1198cb6d46517a8cbb1f62064dd15d39346c2fd2
qcy <464813006@qq.com>
2019-08-23 22:22:46 +0800

    1

--- code.cpp
+++ code.cpp
@@ -1663,3 +1663,37 @@ for i= k+1 to N
     if( M < k)  
      SWAP the Mth value and ith value  
 end for   
+
+53. 求众数
+找2个出现次数超过n/3
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int> res;
+        int m= 0,n=0,cm=0,cn=0;
+        for(auto a : nums){
+            if(a==m)
+                ++cm;
+            else if(a==n)
+                ++cn;
+            else if(cm==0)
+                m = a, cm = 1;
+            else if(cn == 0)
+                n = a, cn = 1;
+            else
+                --cm, --cn;
+        }
+        cm = cn = 0;
+        for(auto a : nums){
+            if(a==m)    
+                ++cm;
+            else if(a==n)
+                ++cn;
+        }
+        if(cm > nums.size()/3 )
+            res.push_back(m);
+        if(cn > nums.size()/3 )
+            res.push_back(n);
+        return res;
+    }
+};
